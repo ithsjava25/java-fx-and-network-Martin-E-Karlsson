8c1ccd800c7d3f0a267c51d314d93448e5e7430b
Martin Karlsson <m.karlsson91@gmail.com>
2025-11-04 02:55:55 +0100

    Messaging application lecture 2025-11-03

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module hellofx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.net.http;
+    requires io.github.cdimascio.dotenv.java;
+    requires tools.jackson.databind;
 
     opens com.example to javafx.fxml;
     exports com.example;
